1703dbb1a668b967dbe2307285696dbaa551ea07
dianbsi <129382031+dianbsi@users.noreply.github.com>
2023-03-30 16:50:58 +0700

    Contoh1

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo "Nama saya Dian Ardiansyah Saya tingga di daerah Karawang olah raga yang saya sukai adalah Futsal";
+ }
+}
